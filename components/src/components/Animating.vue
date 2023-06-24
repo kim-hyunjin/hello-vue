f832c0feedd86588b5c435845078f346d5320130
kim-hyunjin <hyunjin1612@gmail.com>
2023-06-24 18:16:22 +0900

    css animation

--- components/src/components/Animating.vue
+++ components/src/components/Animating.vue
@@ -1,7 +1,16 @@
 <template>
   <button @click="flag = !flag">Toggle</button>
-  <transition name="fade">
-    <h2 v-if="flag">Hello world</h2>
+  <transition name="fade" mode="out-in">
+    <h2 v-if="flag" key="1">Hello fade</h2>
+    <h2 v-else key="2">Another fade</h2>
+  </transition>
+
+  <transition name="zoom">
+    <h2 v-if="flag">Zoom</h2>
+  </transition>
+
+  <transition name="zoom" appear>
+    <h2>Zoom amimate at first</h2>
   </transition>
 </template>
 
@@ -20,13 +29,44 @@ export default {
   opacity: 0;
 }
 .fade-enter-active {
-  transition: all 0.25s linear;
+  transition: all 1s linear;
 }
 .fade-enter-to {
   opacity: 1;
 }
 .fade-leave-to {
-  transition: all 0.25s linear;
+  transition: all 1s linear;
   opacity: 0;
+}
+
+h2 {
+  width: 400px;
+  padding: 20px;
+  margin: 20px;
+}
+
+.zoom-enter-active {
+  animation: zoom-in 1s linear forwards;
+}
+
+.zoom-leave-active {
+  animation: zoom-out 1s linear forwards;
+}
+
+@keyframes zoom-in {
+  from {
+    transform: scale(0, 0);
+  }
+  to {
+    transform: scale(1, 1);
+  }
+}
+@keyframes zoom-out {
+  from {
+    transform: scale(1, 1);
+  }
+  to {
+    transform: scale(0, 0);
+  }
 }
 </style>
