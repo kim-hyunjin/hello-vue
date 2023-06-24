70fb8e67bfc50286077014f072bff97e795764b1
kim-hyunjin <hyunjin1612@gmail.com>
2023-06-24 17:11:49 +0900

    slots

--- components/src/App.vue
+++ components/src/App.vue
@@ -2,21 +2,37 @@
   <h3>Hey!</h3>
   <Greeting :age="age" />
   <user :age="age" @age-change="updateAge" :ageChangeFn="updateAgeCB"></user>
+  <app-form>
+    <template v-slot:help>
+      <p>{{ help }}</p>
+    </template>
+    <template v-slot:fields>
+      <input type="text" />
+      <input type="text" />
+    </template>
+    <template v-slot:buttons>
+      <button>button</button>
+    </template>
+    <p>this is goes to default slot</p>
+  </app-form>
 </template>
 
 <script>
 import Greeting from '@/components/Greeting.vue';
 import User from '@/components/User.vue';
+import AppForm from '@/components/Form.vue';
 
 export default {
   name: 'MyApp',
   components: {
     Greeting,
     User,
+    AppForm,
   },
   data() {
     return {
       age: 20,
+      help: 'this is some help text.',
     };
   },
   methods: {
