651fe1b1e425c171d2442763819b8d4b604a16c8
kim-hyunjin <hyunjin1612@gmail.com>
2023-06-25 17:55:56 +0900

    feat: multiple error msg using v-slot

--- music-app/src/components/AppAuth.vue
+++ music-app/src/components/AppAuth.vue
@@ -118,13 +118,17 @@
             <!-- Password -->
             <div class="mb-3">
               <label class="inline-block mb-2">Password</label>
-              <vee-field
-                type="password"
-                name="password"
-                class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
-                placeholder="Password"
-              />
-              <ErrorMessage class="text-red-600" name="password" />
+              <vee-field name="password" :bails="false" v-slot="{ field, errors }">
+                <input
+                  type="password"
+                  class="block w-full py-1.5 px-3 text-gray-800 border border-gray-300 transition duration-500 focus:outline-none focus:border-black rounded"
+                  placeholder="Password"
+                  v-bind="field"
+                />
+                <div class="text-red-600" v-for="error in errors" :key="error">
+                  {{ error }}
+                </div>
+              </vee-field>
             </div>
             <!-- Confirm Password -->
             <div class="mb-3">
@@ -190,7 +194,7 @@ export default defineComponent({
         name: 'required|min:3|max:100|alpha_spaces',
         email: 'required|email',
         age: 'required|min_value:18|max_value:100',
-        password: 'required|min:3|max:100',
+        password: 'required|min:9|max:100|not_one_of:password',
         confirmPassword: 'confirmed:@password',
         country: 'required|not_one_of:Antarctica',
         tos: 'required'
