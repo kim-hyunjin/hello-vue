304f3178aa5662154fd384fc048bbefcbf0b65f4
kim-hyunjin <hyunjin1612@gmail.com>
2023-06-25 17:41:33 +0900

    feat: register form validation

--- music-app/src/includes/validation.ts
+++ music-app/src/includes/validation.ts
@@ -1,9 +1,31 @@
 import type { Plugin } from 'vue'
-import { Form as VeeForm, Field as VeeField } from 'vee-validate'
+import { Form as VeeForm, Field as VeeField, defineRule, ErrorMessage } from 'vee-validate'
+import {
+  required,
+  min,
+  max,
+  alpha_spaces,
+  email,
+  min_value,
+  max_value,
+  confirmed,
+  not_one_of
+} from '@vee-validate/rules'
 
 export default {
   install(app, option) {
     app.component('VeeForm', VeeForm)
     app.component('VeeField', VeeField)
+    app.component('ErrorMessage', ErrorMessage)
+
+    defineRule('required', required)
+    defineRule('min', min)
+    defineRule('max', max)
+    defineRule('alpha_spaces', alpha_spaces)
+    defineRule('email', email)
+    defineRule('min_value', min_value)
+    defineRule('max_value', max_value)
+    defineRule('confirmed', confirmed)
+    defineRule('not_one_of', not_one_of)
   }
 } as Plugin
