e9f399e81f969fe8bdb8774842b9d4f9ed8fb49f
kim-hyunjin <hyunjin1612@gmail.com>
2023-07-01 17:02:30 +0900

    feat: set active link class

--- music-app/src/router/index.ts
+++ music-app/src/router/index.ts
@@ -19,7 +19,8 @@ const routes: Readonly<RouteRecordRaw[]> = [
 ]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes
+  routes,
+  linkExactActiveClass: 'text-yellow-500'
 })
 
 export default router
