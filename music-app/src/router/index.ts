a02ce546f92dd35d90b6971a0f717f214b71830f
kim-hyunjin <hyunjin1612@gmail.com>
2023-07-01 16:58:13 +0900

    feat: router 적용, manage page 추가

--- music-app/src/router/index.ts
+++ music-app/src/router/index.ts
@@ -1,8 +1,25 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHistory, type RouteRecordRaw } from 'vue-router'
+import HomeView from '@/views/HomeView.vue'
+import AboutView from '@/views/AboutView.vue'
+import ManageView from '@/views/ManageView.vue'
 
+const routes: Readonly<RouteRecordRaw[]> = [
+  {
+    path: '/',
+    component: HomeView
+  },
+  {
+    path: '/about',
+    component: AboutView
+  },
+  {
+    path: '/manage',
+    component: ManageView
+  }
+]
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
-  routes: []
+  routes
 })
 
 export default router
