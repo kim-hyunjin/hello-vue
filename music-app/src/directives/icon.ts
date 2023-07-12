78c00426f1117840d43f02fab8a614b328ba88f2
khj-dev <khj@devtools.kr>
2023-07-12 17:37:23 +0900

    feat: icon directive with more modifiers

--- music-app/src/directives/icon.ts
+++ music-app/src/directives/icon.ts
@@ -2,7 +2,19 @@ import type { Directive } from 'vue'
 
 export default {
   beforeMount(el, binding) {
-    let iconClass = `fa fa-${binding.value} float-right text-green-400 text-xl`
+    let iconClass = `fa fa-${binding.value} text-xl`
+
+    if (binding.modifiers.right) {
+      iconClass += ' float-right'
+    } else if (binding.modifiers.left) {
+      iconClass += ' float-left'
+    }
+
+    if (binding.modifiers.yellow) {
+      iconClass += ' text-yellow-400'
+    } else {
+      iconClass += ' text-green-400'
+    }
 
     if (binding.arg === 'full') {
       iconClass = binding.value
