bc4a8f0b7e1f050b38bcb920914ffee5bcd801ef
kim-hyunjin <hyunjin1612@gmail.com>
2023-07-13 22:22:07 +0900

    feat: config workbox for caching

--- music-app/vite.config.ts
+++ music-app/vite.config.ts
@@ -27,6 +27,9 @@ export default defineConfig({
             type: 'image/png'
           }
         ]
+      },
+      workbox: {
+        globPatterns: ['**/*.{js,css,html,png,jpg,ts}']
       }
     })
   ],
