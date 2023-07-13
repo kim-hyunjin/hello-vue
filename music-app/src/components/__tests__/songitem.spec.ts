dc4ac252c8a9941ee108463c4fd8856fb516008b
khj-dev <khj@devtools.kr>
2023-07-13 17:24:59 +0900

    feat: add test fixture, setup i18n for test

--- music-app/src/components/__tests__/songitem.spec.ts
+++ music-app/src/components/__tests__/songitem.spec.ts
@@ -2,19 +2,11 @@ import type { SongWithID } from '@/models/song'
 import SongItemVue from '../SongItem.vue'
 import { shallowMount, RouterLinkStub } from '@vue/test-utils'
 import { describe, expect, test } from 'vitest'
+import { dummySongs } from '../../fixture'
 
 describe('SongItem.vue', () => {
   test('renders song.display_name', () => {
-    const song: SongWithID = {
-      display_name: 'test',
-      comment_count: 0,
-      doc_id: 'test',
-      genre: 'test',
-      modified_name: 'test',
-      original_name: 'test',
-      uid: 'test',
-      url: 'test'
-    }
+    const song: SongWithID = dummySongs[0]
     const wrapper = shallowMount(SongItemVue, {
       props: { song },
       global: {
--- music-app/vite.config.ts
+++ music-app/vite.config.ts
@@ -6,7 +6,8 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   test: {
-    globals: true
+    globals: true,
+    setupFiles: 'vitest.setup.ts'
   },
   plugins: [vue()],
   resolve: {
